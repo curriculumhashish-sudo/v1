9839de4d2333117e5ec0f7e43b496052beb71c5c
zhangxiaowei <zhangxiaowei@virtaitech.com>
2026-07-07 20:12:34 +0800

    B改注释

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-    // 1111workspaceA 添加的功能
+    // 1111workspaceB222 添加的功能
     public class HelloWorld {
         public static void main(String[] args) {
             System.out.println("Hello World");
